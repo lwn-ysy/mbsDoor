a14301a05b8db6c0317e20fc464445f76cdfe67f
lwn-ysy <631955411@qq.com>
2021-08-29 21:29:54 +0800

    增加现场施工blog界面

--- pages/show/show.js
+++ pages/show/show.js
@@ -5,14 +5,34 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tab:[['深圳南山','中山'],['东莞凤岗','梅州兴宁'],['香港','深圳龙岗'],['深圳坪山','梅州五华'],['河源','惠州仲恺'],['河源','惠州仲恺'],['河源','惠州仲恺']],
+    animate:false,
   },
-
+  search: function (value) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve([{
+          text: '搜索逻辑待定',
+          value: 1
+        }, {
+          text: '待做',
+          value: 2
+        }])
+      }, 200)
+    })
+  },
+  // 点击搜索内的值
+  selectResult(e) {
+    console.log(e);
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    //搜索框的值
+    this.setData({
+      search: this.search.bind(this)
+    })
   },
 
   /**
